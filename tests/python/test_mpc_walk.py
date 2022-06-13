c4648d260c9f959f96e2c406d56c30ab3f8dcfea
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-06-14 00:10:59 +0200

    black

--- tests/python/test_mpc_walk.py
+++ tests/python/test_mpc_walk.py
@@ -4,7 +4,7 @@ import numpy as np
 import pinocchio as pin
 import sobec
 
-robot = robex.load('ur5')
+robot = robex.load("ur5")
 model = robot.model
 
 runmodels = []
@@ -15,18 +15,17 @@ actuation = croc.ActuationModelFloatingBase(state)
 # Costs
 costs = croc.CostModelSum(state, actuation.nu)
 
-model.x0 = np.concatenate([ pin.neutral(model), np.zeros(model.nv) ])
+model.x0 = np.concatenate([pin.neutral(model), np.zeros(model.nv)])
 xRegResidual = croc.ResidualModelState(state, model.x0, actuation.nu)
-xRegCost = croc.CostModelResidual(state,xRegResidual)
+xRegCost = croc.CostModelResidual(state, xRegResidual)
 costs.addCost("stateReg", xRegCost, 1)
 
-damodel = croc.DifferentialActionModelFreeFwdDynamics( state, actuation,costs )
-amodel = croc.IntegratedActionModelEuler(damodel, .1)
+damodel = croc.DifferentialActionModelFreeFwdDynamics(state, actuation, costs)
+amodel = croc.IntegratedActionModelEuler(damodel, 0.1)
 
-problem = croc.ShootingProblem(model.x0,[ amodel for t in range(20) ],amodel)
+problem = croc.ShootingProblem(model.x0, [amodel for t in range(20)], amodel)
 
 mpc = sobec.MPCWalk(problem)
-mpc.Tmpc = 10 #len(problem.runningModels)
-mpc.initialize([ model.x0,model.x0 ],[model.x0])
-mpc.calc(model.x0,0)
-
+mpc.Tmpc = 10  # len(problem.runningModels)
+mpc.initialize([model.x0, model.x0], [model.x0])
+mpc.calc(model.x0, 0)
